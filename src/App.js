10a4f9ad30219226a06572f2834dbba32d0e08da
Zachary Beall <47151377+pngsavvy@users.noreply.github.com>
2021-06-05 20:36:54 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import get_doge_price from './get_price';
+import React, {useState, useEffect} from "react"
+
+// export default class UserComponent extends React.Component {
+//   constructor(){
+//     super()
+//     this.state={
+//       doge_price: 0.0,
+//       isLoaded: false,
+//     }
+//   }
+
+//   componentDidMount(){
+//     var price = () => get_doge_price()
+//     this.setState({
+//       doge_price: price,
+//       isLoaded: true,
+//     })
+//     console.log("<<<<<")
+//     console.log(price)
+//   }
+//   render(){
+//     if(!this.state.isLoaded) {
+//       return <div class="App">Loading...</div>
+//     }
+//     else{
+//       return <div class ="App">hi  {this.state.doge_price}  {0}</div>
+//     }
+//   }
+// }
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [price, setPrice] = useState(0.0);
+  const doge_coins_owned = 4000000
+  useEffect(() => {
+    get_price()
+  }, [])
+
+  const get_price = () => get_doge_price().then(result => {
+    setPrice((result * doge_coins_owned).toFixed(2))
+  });
+
+  console.log(price)
+
+
+  if(!price){
+    return (<div>Loading...</div>)
+  } else {
+    return (
+    <div class="App">
+      <h2>@ProTheDoge's Portfolio</h2>
+      <h2>Dogecoins: {doge_coins_owned}</h2>
+      <h1>${price}</h1>
+    
+    </div>)
+  }
+  
 }
 
 export default App;
---
+++ src/get_price.js
@@ -0,0 +1,49 @@
+import Axios from 'axios'
+
+export default function get_doge_price(){
+    console.log("getting price")
+    var token = ""
+    var price = 0.0
+    // Request Token
+    const auth_response = {
+    method: 'POST',
+    url: 'https://bravenewcoin.p.rapidapi.com/oauth/token',
+    headers: {
+      'content-type': 'application/json',
+      'x-rapidapi-key': 'c82e54e894mshf547db01668712dp16354ajsnf9b2b32ee16d',
+      'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com'
+    },
+    data: {
+      audience: 'https://api.bravenewcoin.com',
+      client_id: 'oCdQoZoI96ERE9HY3sQ7JmbACfBf55RY',
+      grant_type: 'client_credentials'
+    }
+  };
+
+  price =  Axios.request(auth_response).then(function (auth_response) {
+      token = auth_response.data.access_token 
+
+      // Use Token to get price
+      const coin_info_options = {
+          method: 'GET',
+          url: 'https://bravenewcoin.p.rapidapi.com/market-cap',
+          params: {assetId: '7bb2339e-b6eb-408c-836f-2894c8751c6d'},
+          headers: {
+            authorization: `Bearer ${token}`,
+            'x-rapidapi-key': 'c82e54e894mshf547db01668712dp16354ajsnf9b2b32ee16d',
+            'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com'
+          }
+        };
+        
+        return Axios.request(coin_info_options).then( response => {
+          return response.data.content[0].price
+        }).catch(function (error) {
+          console.error(error);
+        });
+
+      }).catch(function (error) {
+        console.error(error);
+      });
+      console.log(price)
+      return price
+  } 
